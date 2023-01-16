0bac917204eec2b35dadfecfd547cc75da4358b6
Aaryen Cyril D'souza <dc.aaryen@gmail.com>
2023-01-16 13:26:41 +0530

    Changed mongodb url to application

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/maharsh-toast-api", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://aaryen:hahahee@cluster0.6ltzhmk.mongodb.net/maharsh-toast-api?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
 
-// mongodb+srv://aaryen:<hahahee>@cluster0.6ltzhmk.mongodb.net/test
+// mongodb://127.0.0.1:27017/maharsh-toast-api
